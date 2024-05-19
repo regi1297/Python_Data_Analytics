d4e5c10fb3389dc8071171b8012a7c61335e9e25
regi1297 <reginald_miranda@hotmail.com>
2024-05-19 13:40:38 -0300

    Utilizando print e input

---
+++ Primeiro_programa/print_input.py
@@ -0,0 +1,10 @@
+nome = input("Informe o seu nome: ")
+
+idade = int(input("Informe a sua idade: "))
+print(nome, idade)
+
+print(nome, idade, end=" ... \n")
+
+print(nome, idade, sep="#", end=" ... \n")
+
+print(nome, idade, sep="#")
