78d8756a28d3746093da9dee282d1d56605ce443
regi1297 <reginald_miranda@hotmail.com>
2024-05-19 13:31:19 -0300

    tipos e variáveis

---
+++ Primeiro_programa/convertendo_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.9))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+print(str(10.10))
+print(type(str(10.10)))
+
+valor = 10
+valor_str = str(valor)
+
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
---
+++ Primeiro_programa/variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = "Reginaldo"
+
+idade = 40
+
+nome, idade = "Reginaldo", 41
+
+print (nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+BRAZILIAN_STATES = 10
+
+print(BRAZILIAN_STATES)
