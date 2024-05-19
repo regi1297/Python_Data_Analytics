d1dd1077bc0c20aad1e6398f46bb8b093fe499a6
regi1297 <reginald_miranda@hotmail.com>
2024-05-19 15:17:22 -0300

    Tipos de operadores

---
+++ Tipos de Operadores com Python/operdadores_atribuicao.py
@@ -0,0 +1,29 @@
+saldo = 500
+print("Saldo inicial: ", saldo)
+saldo = 200
+print("Saldo inicial: ", saldo)
+
+
+
+saldo += 10
+print("Saldo: ", saldo)
+
+
+
+saldo -= 5
+print("Saldo: ", saldo)
+
+saldo //= 2
+print("Saldo: ", saldo)
+
+saldo /= 2
+print("Saldo: ", saldo)
+
+saldo *= 10
+print("Saldo: ", saldo)
+
+saldo %= 4
+print("Saldo: ", saldo)
+
+saldo **= 2
+print("Saldo: ", saldo)
