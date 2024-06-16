3c379affc8b113b3d29a70551091b10594726fdf
regi1297 <reginald_miranda@hotmail.com>
2024-06-15 21:13:28 -0300

    string

---
+++ Manipulando Strings com Python/string_1.py
@@ -0,0 +1,20 @@
+nome = "Reginaldo"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = " olá mundo!   "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("###" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
+
